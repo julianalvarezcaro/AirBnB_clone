d22bb8a4b4939e50428594dc51431b536be8ec09
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-29 19:08:14 -0500

    update __init__ method | init with dict

--- models/base_model.py
+++ models/base_model.py
@@ -25,13 +25,20 @@ class BaseModel():
         - __init__(self)
         - __str__(self)
     """
-    def __init__(self):
+    def __init__(self, *_args, **kwargs):
         """
         Initializes an instance of BaseModel class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key not in ["__class__", "created_at", "updated_at"]:
+                    self.__dict__[key] = value
+            self.__dict__['created_at'] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.__dict__['updated_at'] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
